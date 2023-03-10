861b3b52b99385e89984652426cae18538885e38
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-03-10 15:43:56 -0500

    micropayments & lightning article

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,29 @@
 <script>
 	import '../app.css';
+	import Icon from '@iconify/svelte';
 </script>
 
-<div class="prose mx-auto">
-	<slot />
+<div class="flex flex-col gap-8 prose justify-center items-center mx-auto mt-10">
+	<div class="flex w-full justify-around">
+		<a class="text-3xl no-underline hover:text-gray-500 transition-all font-bold" href="/"
+			>Brandon Lucas</a
+		>
+		<div class="flex gap-2">
+			<a
+				class="text-4xl hover:text-gray-500 transition-all"
+				target="_blank"
+				rel="noreferrer"
+				href="https://github.com/thebrandonlucas"><Icon icon="mdi:github" /></a
+			>
+			<a
+				class="text-4xl hover:text-gray-500 transition-all"
+				target="_blank"
+				rel="noreferrer"
+				href="https://twitter.com/brandonstlucas"><Icon icon="mdi:twitter" /></a
+			>
+		</div>
+	</div>
+	<div>
+		<slot />
+	</div>
 </div>
---
+++ src/routes/+page.md
@@ -0,0 +1,3 @@
+### [Micropayments and the Lightning Network](/posts/lightning/micropayments)
+
+_March 10, 2023_
